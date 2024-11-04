62bf9d527bb9ad847c402f397caeb9bf2f44646d
Matias <soporte_avgn@icloud.com>
2024-11-04 18:27:59 -0300

    Add files via upload

---
+++ src/matty/LangManager/LangCommand.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace matty\LangManager;
+
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\player\Player;
+
+/**
+ * Class LangCommand
+ *
+ * Handles the /lang command, allowing players to set their preferred language
+ * from a list of available languages.
+ */
+class LangCommand extends Command{
+	
+	public function __construct(Main $plugin){
+		parent::__construct("lang", "Set your preferred language", "/lang <language>", ["language"]);
+	}
+	
+	/**
+	 * Executes the /lang command.
+	 *
+	 * @param CommandSender $sender
+	 * @param string $commandLabel
+	 * @param array $args
+	 * @return bool
+	 */
+	public function execute(CommandSender $sender, string $commandLabel, array $args): bool{
+		if(!$sender instanceof Player){
+			$sender->sendMessage(LangManager::translate("error_no_permission", $sender));
+			return false;
+		}
+		
+		$languages = [];
+		foreach(LangManager::ALL_ISO_CODES as $language => $iso){
+			$languages[] = LangManager::translate("language_list", $sender, $language, $iso);
+		}
+		$languages = implode(", ", $languages);
+		
+		if(count($args) < 1){
+			$sender->sendMessage(LangManager::translate("language_choose", $sender) . "\n" . $languages);
+			return false;
+		}
+		
+		$langManager = LangManager::getInstance();
+		
+		$iso = strtolower($args[0]);
+		if(!$langManager->isLanguageAvailable($iso)){
+			// Send error message for invalid language selection
+			$sender->sendMessage(LangManager::translate("error_invalid_language", $sender));
+			return false;
+		}
+		
+		$langManager->setPlayerLanguage($sender, $iso);
+        // Send confirmation message when the language is set
+		$sender->sendMessage(LangManager::translate("language_set", $sender));
+		return true;
+	}
+}
---
+++ src/matty/LangManager/Main.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace matty\LangManager;
+
+use pocketmine\plugin\PluginBase;
+
+/**
+ * Class Main
+ *
+ * This class serves as the main plugin class for the LangManager plugin.
+ */
+class Main extends PluginBase{
+	
+	public const MAXMIND_DB_RESOURCE = "GeoLite2-Country.mmdb";
+	
+	/**
+	 * @var Main|null $instance
+	 */
+	private static ?Main $instance = null;
+	
+	/**
+	 * Gets the singleton instance of the Main class.
+	 *
+	 * @return Main|null The current instance of Main, or null if not set.
+	 */
+	public static function getInstance(): ?self{
+		return self::$instance;
+	}
+	
+	/**
+	 * @return void
+	 */
+	public function onEnable(): void{
+		self::$instance = $this;
+		$this->saveResource(self::MAXMIND_DB_RESOURCE, true);
+		$this->saveResource("lang/en.ini", true);
+		$this->saveResource("lang/es.ini", true);
+		
+		$this->getServer()->getCommandMap()->register("langmanager", new LangCommand($this));
+		
+		new LangManager($this);
+		$this->getLogger()->info("LangManager enabled.");
+	}
+	
+	/**
+	 * @return void
+	 */
+	public function onDisable(): void{
+		LangManager::close();
+		$this->getLogger()->info("LangManager disabled.");
+	}
+	
+}
